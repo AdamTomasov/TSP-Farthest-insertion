e24371f082af81e5a9f09e3c0b74830397f935e8
Adam Juraj Tomasov <adamjurajtomasov@gmail.com>
2018-11-27 23:00:00 +0100

    update

--- FarthestInsertion.py
+++ FarthestInsertion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import numpy as np
 
 def open_file(data):
     df = pd.read_csv(data, sep=" ", header=None)
@@ -16,12 +17,41 @@ def removeDuplicates(listofElements):
 
 data = open_file('data.txt')
 
+
 #odfiltrovanie uzlov
 
 cities = []
 cities = data[0].tolist() + data[1].tolist()
 cities = removeDuplicates(cities)
 r_city = random.sample(cities, 1)
+print(r_city)
 
-print(cities)
+# vyber j uzlov
+
+l = []
+for index,row in data.iterrows():
+    if row[0] == r_city or row[1] == r_city:
+       l.append(row)
+
+selected_data = pd.DataFrame(l)
+
+
+tour = []
+tour = selected_data.where(selected_data[2] == min(selected_data[2])).dropna()
+tour = tour.values[0, :2].tolist()
+tour = [int(i) for i in tour]
+print(tour)
+
+
+others = [x for x in cities if x not in tour]
+print(others)
+
+
+
+
+
+
+
+
+
 
